1b2e776d506e2465659455ca0731d8f10b4bddc5
João Vitor Silva <jvs0712@hotmail.com>
2024-11-22 20:50:56 -0300

    Primeiro commit

---
+++ src/MeuComponente.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MeuComponente() {
+  return (
+    <div className="meu-componente">
+      <h2>Sobreviventes: A Jornada de Joel e Ellie</h2>
+      <p>O mundo foi devastado pela infecção, mas ainda há esperança. Joel e Ellie são sobreviventes tentando atravessar o país em busca de segurança.</p>
+      <ul>
+        <li>Infectados: Criaturas que perderam sua humanidade.</li>
+        <li>Factions: Os sobreviventes humanos que lutam entre si.</li>
+        <li>A Esperança: A jornada para salvar a humanidade.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MeuComponente;
